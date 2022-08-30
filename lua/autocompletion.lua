eebafaa496dbcbca106ccc285863b46f2f7b0e33
jobypsunny <jobypsunny@factweavers.com>
2022-08-30 19:31:06 +0530

    Updated Priority in autocompletion

--- lua/autocompletion.lua
+++ lua/autocompletion.lua
@@ -15,8 +15,8 @@ cmp.setup({
         end
     },
     sources = {
-        { name = 'path' },
         { name = 'nvim_lsp', keyword_length = 3 },
+        { name = 'path' },
         { name = 'buffer', keyword_length = 3 },
         { name = 'luasnip', keyword_length = 2 },
     },
