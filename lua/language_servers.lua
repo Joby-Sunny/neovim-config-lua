3972d46282577d860116e41c67014f65cbe77295
jobypsunny <jobypsunny@factweavers.com>
2023-02-09 15:09:46 +0530

    Added emmet to the config. NB: Installing lsp servers in not evident from the setup. Need to add this to README.md

--- lua/language_servers.lua
+++ lua/language_servers.lua
@@ -9,7 +9,7 @@ local lsp_package = 'lspconfig'
 
 -- initiate tsserver
 local function start_tsserver(language_server)
-    local capabilities = require(completion_package).update_capabilities(vim.lsp.protocol.make_client_capabilities())
+    local capabilities = require(completion_package).default_capabilities(vim.lsp.protocol.make_client_capabilities())
     require(lsp_package)[language_server].setup {
         capabilities = capabilities,
         on_attach = on_attach,
@@ -19,7 +19,7 @@ end
 
 local function start_cssls(language_server)
     --Enable (broadcasting) snippet capability for completion
-    local capabilities = require(completion_package).update_capabilities(vim.lsp.protocol.make_client_capabilities())
+    local capabilities = require(completion_package).default_capabilities(vim.lsp.protocol.make_client_capabilities())
     capabilities.textDocument.completion.completionItem.snippetSupport = true
 
     require(lsp_package)[language_server].setup {
@@ -31,12 +31,12 @@ end
 
 local function start_cssmodules_ls(language_server)
     --Enable (broadcasting) snippet capability for completion
-    local capabilities = require(completion_package).update_capabilities(vim.lsp.protocol.make_client_capabilities())
+    local capabilities = require(completion_package).default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
     require(lsp_package)[language_server].setup {
         on_attach = function(client, buffnr)
             -- avoid accepting `go-to-definition` responses from this LSP
-            client.resolved_capabilities.goto_definition = false
+            client.server_capabilities.goto_definition = false
             on_attach(client, buffnr)
         end,
         capabilities = capabilities,
@@ -46,7 +46,7 @@ end
 
 local function start_html(language_server)
     --Enable (broadcasting) snippet capability for completion
-    local capabilities = require(completion_package).update_capabilities(vim.lsp.protocol.make_client_capabilities())
+    local capabilities = require(completion_package).default_capabilities(vim.lsp.protocol.make_client_capabilities())
     capabilities.textDocument.completion.completionItem.snippetSupport = true
 
     require(lsp_package)[language_server].setup {
@@ -58,7 +58,7 @@ end
 
 local function start_jsonls(language_server)
     --Enable (broadcasting) snippet capability for completion
-    local capabilities = require(completion_package).update_capabilities(vim.lsp.protocol.make_client_capabilities())
+    local capabilities = require(completion_package).default_capabilities(vim.lsp.protocol.make_client_capabilities())
     capabilities.textDocument.completion.completionItem.snippetSupport = true
 
     require(lsp_package)[language_server].setup {
@@ -70,7 +70,7 @@ end
 
 local function start_eslint(language_server)
     --Enable (broadcasting) snippet capability for completion
-    local capabilities = require(completion_package).update_capabilities(vim.lsp.protocol.make_client_capabilities())
+    local capabilities = require(completion_package).default_capabilities(vim.lsp.protocol.make_client_capabilities())
     capabilities.textDocument.completion.completionItem.snippetSupport = true
 
     require(lsp_package)[language_server].setup {
@@ -80,10 +80,30 @@ local function start_eslint(language_server)
     }
 end
 
+local function start_emmet(language_server)
+  local capabilities = require(completion_package).default_capabilities(vim.lsp.protocol.make_client_capabilities())
+  capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+  require(lsp_package)[language_server].setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+    filetypes = { 'html', 'typescriptreact', 'javascriptreact', 'css', 'sass', 'scss', 'less' },
+    init_options = {
+      html = {
+        options = {
+          -- For possible options, see: https://github.com/emmetio/emmet/blob/master/src/config.ts#L79-L267
+          ["bem.enabled"] = true,
+        },
+      },
+    }
+  }
+end
+
 -- invoking servers
 start_tsserver('tsserver');
 start_cssls('cssls');
 start_cssmodules_ls('cssmodules_ls');
-start_html('html')
-start_jsonls('jsonls')
-start_eslint('eslint')
+start_html('html');
+start_jsonls('jsonls');
+start_eslint('eslint');
+start_emmet('emmet_ls');
